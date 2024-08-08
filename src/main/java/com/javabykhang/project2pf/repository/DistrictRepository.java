462ca9befbf3d224a7e57df23639310de1efbc19
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2024-08-08 19:59:56 +0700

    spring datajpa

---
+++ src/main/java/com/javabykhang/project2pf/repository/DistrictRepository.java
@@ -0,0 +1,7 @@
+package com.javabykhang.project2pf.repository;
+
+import com.javabykhang.project2pf.repository.entity.DistrictEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistrictRepository extends JpaRepository<DistrictEntity, Long> {
+}
---
+++ src/main/java/com/javabykhang/project2pf/repository/RentareaRepository.java
@@ -0,0 +1,7 @@
+package com.javabykhang.project2pf.repository;
+
+import com.javabykhang.project2pf.repository.entity.RentareaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentareaRepository extends JpaRepository<RentareaEntity, Long> {
+}
